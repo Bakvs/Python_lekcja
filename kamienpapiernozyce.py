34d0e07707e9aacaf531c1727baa3186806aac0e
Bakvs <jakubwawrzen2008@gmail.com>
2024-04-23 10:23:20 +0200

    .

--- kamienpapiernozyce.py
+++ kamienpapiernozyce.py
@@ -1,21 +1,20 @@
 import random
-
-def gracz():
-    gracz = input("Wybierz kamień, papier lub nożyce").lower()
-    while gracz not in ['kamień', 'papier', 'nożyce']:
+def wyborgracza():
+    gracz = input("Wybierz kamień, papier lub nożyce: ")
+    while gracz not in ["kamień", "papier", "nożyce"]:
         print("Niepoprawny wybór, spróbuj ponownie.")
-        gracz = input("Wybierz kamień, papier lub nożyce").lower()
+        gracz = input("Wybierz kamień, papier lub nożyce: ")
     return gracz
 
-def komputer():
-    return random(['kamień', 'papier', 'nożyce'])
+def wyborkomputera():
+    return random.choice(["kamień", "papier", "nożyce"])
 
 def gra(gracz, komputer):
     if gracz == komputer:
         return "REMIS"
-    elif (gracz == 'papier' and komputer == 'kamień') or \
-         (gracz == 'kamień' and komputer == 'nożyce') or \
-         (gracz == 'nożyce' and komputer == 'papier'):
+    elif (gracz == "papier" and komputer == "kamień") or \
+          (gracz == "kamień" and komputer == "nożyce") or \
+          (gracz == "nożyce" and komputer == "papier"):
         return "WYGRANA"
     else:
         return "PRZEGRANA"
@@ -23,14 +22,16 @@ def gra(gracz, komputer):
 def main():
     print("Witaj w grze kamień, papier lub nożyce")
     while True:
-        gracz = gracz()
-        komputer = komputer()
-        print("Twój wybór: {gracz}")
-        print("Wybor komputera: {komputer}")
+        gracz = wyborgracza()
+        komputer = wyborkomputera()
+        print("Twój wybór: " + gracz)
+        print("Wybor komputera: " + komputer)
         print(gra(gracz, komputer))
-        play_again = input("Chcesz zagrać jeszcze raz? (tak/nie): ").lower()
-        if play_again == 'tak':
-            return main
+        play_again = input("Czy chcesz zagrać jeszcze raz? : ")
+        if play_again == "tak":
+            return main()
         else:
-            print("Dziękujemy za grę!")
+            print("Dziękujemy za grę")
             break
+if __name__ == "__main__":
+    main()
